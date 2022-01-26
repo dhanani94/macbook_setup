7978a74002bf2738221079e1ff749f26e0bb9386
Taufiq <dhanani94@gmail.com>
2022-01-26 09:22:50 -0500

    adds brew to path

--- 0_install_packages.sh
+++ 0_install_packages.sh
@@ -2,6 +2,8 @@
 
 echo "installing homebrew"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
 
 echo "installing a bunch of missing packages"
 brew install exiftool
